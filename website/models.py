9be2f026c0d0b29ceff6a8a1155e754f2de1d70b
princesshavy <havilah757@gmail.com>
2025-03-02 15:48:04 +0000

    added havilah model with email and password tables

--- website/models.py
+++ website/models.py
@@ -18,3 +18,12 @@ class Website(models.Model):
 
     def __str__(self):
         return self.title
+
+class Havilah(models.Model):
+    email = models.EmailField(max_length=20)
+    password = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
